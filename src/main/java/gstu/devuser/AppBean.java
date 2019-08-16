11285ec47f6c6052fc36a80d739c6d342b12f2e5
buldozer <123anice@mail.ru>
2019-08-16 15:50:07 +0300

    Add mvn

---
+++ src/main/java/gstu/devuser/AppBean.java
@@ -0,0 +1,11 @@
+package gstu.devuser;
+public class AppBean
+{
+private String text;
+public AppBean(final String text) {
+this.text = text;
+}
+public String sayHello() {
+return "Hello, " + text;
+}
+}
